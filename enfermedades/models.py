08449dd6070c5167646ff21ec69d1bd869c1ff0d
Arian903 <acanazacua@unsa.edu.pe>
2021-07-10 18:25:01 -0500

    Creando modelo Consejo en la app enfermedades

--- enfermedades/models.py
+++ enfermedades/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Consejos(models.Model):
+    #Titulo del consejo
+    conTit = models.CharField(max_length=40)
+    #Descripcion del consejo
+    conDes = models.TextField()
+    #Url de la imagen
+    conImaUrl = models.TextField()
