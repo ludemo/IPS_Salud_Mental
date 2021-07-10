8fe0ed7204860794ba6e8d08834928df455ae60b
PauChoqueneira <pchoqueneira@unsa.edu.pe>
2021-07-10 18:18:01 -0500

    agregamos la app enfermedades a settings

--- enfermedades/models.py
+++ enfermedades/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Enfermedad(models.Model):
+
+	nombreE = models.CharField(max_length=200)
+	imagenEF = models.ImageField(upload_to='img/enfermedades')
+	descripcionE = models.TextField(max_length=200)
+	estrategia = models.TextField()
+	imagenET = models.ImageField(upload_to='img/enfermedades')
--- enfermedades/urls.py
+++ enfermedades/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static 
-
+ 
 urlpatterns = [
     path('consejos', views.consejos, name = 'consejos'),
     path('depresion', views.depresion, name = 'depresion'),
