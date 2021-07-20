13e65235e5475ff71f1b86f7b0737fceb54c3f68
José Maykol <jpaniura@unsa.edu.pe>
2021-07-20 18:03:30 -0500

    Reestructacion del proyecto

--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from enfermedades.models import Enfermedad
 
 # Create your views here.
 # View basada en función para renderizar el homepage
 def home(request, *args, **kwargs):
-    return render(request, "homepage.html", {})
+    enfermedades = Enfermedad.objects.all()
+    return render(request, "homepage.html", {'enfermedades': enfermedades})
