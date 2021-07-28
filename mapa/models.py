c8927c7edac85eaaea154367ec1f333c222cd117
PauChoqueneira <pchoqueneira@unsa.edu.pe>
2021-07-28 16:24:33 -0500

    elemento 'Hospital donde se atendió el usuario' se corrgió y ya muestra el valor en lugar del id

--- mapa/models.py
+++ mapa/models.py
@@ -3,31 +3,31 @@ from django.db import models
 # Create your models here.
 
 Centros = (
-	(1, 'Hospital Honorio Delgado'),
-	(2, 'Hospital Goyeneche'),
-	(3, 'Clinica de Neuropsiquiatria y Salud Mental San Rafael'),
-	(4, 'Centro de Salud Mental Muñoz'),
-	(5, 'Centro de Salud Mental Comunitario Hunter'),
-	(6, 'El Instituto de Salud Mental Carlos Alberto Seguin Escobedo'),
-	(7, 'Centro de Adicciones Y Salud Mental'),
-	(8, 'Centro de salud mental comunitario San Martin de Porres Arequipa'),
-	(9, 'Clinica de Salud Mental y Adicciones'),
-	(10, 'Centro Psicologico Kid Zone'),
-	(11, 'Centro de Salud Mental y Adicciones Victor Raul Hinojosa'),
-	(12, 'Centro de Salud Mental Comunitario CAYMA'),
-	(13, 'Psicologos Bienestar - Sede Arequipa'),
-	(14, 'Centro de Salud Mental Comunitario Simon Bolivar'),
-	(15, 'Centro Psicologico de la Fuente'),
-	(16, 'Centro Psicologico'),
-	(17, 'Psicomedic-Tratamiento Psiquiatricos'),
-	(18, 'Centro Psicologico - NEURO TALENTO'),
+	('Hospital Honorio Delgado', 'Hospital Honorio Delgado'),
+	('Hospital Goyeneche', 'Hospital Goyeneche'),
+	('Clinica de Neuropsiquiatria y Salud Mental San Rafael', 'Clinica de Neuropsiquiatria y Salud Mental San Rafael'),
+	('Centro de Salud Mental Muñoz', 'Centro de Salud Mental Muñoz'),
+	('Centro de Salud Mental Comunitario Hunter', 'Centro de Salud Mental Comunitario Hunter'),
+	('El Instituto de Salud Mental Carlos Alberto Seguin Escobedo', 'El Instituto de Salud Mental Carlos Alberto Seguin Escobedo'),
+	('Centro de Adicciones Y Salud Mental', 'Centro de Adicciones Y Salud Mental'),
+	('Centro de salud mental comunitario San Martin de Porres Arequipa', 'Centro de salud mental comunitario San Martin de Porres Arequipa'),
+	('Clinica de Salud Mental y Adicciones', 'Clinica de Salud Mental y Adicciones'),
+	('Centro Psicologico Kid Zone', 'Centro Psicologico Kid Zone'),
+	('Centro de Salud Mental y Adicciones Victor Raul Hinojosa', 'Centro de Salud Mental y Adicciones Victor Raul Hinojosa'),
+	('Centro de Salud Mental Comunitario CAYMA', 'Centro de Salud Mental Comunitario CAYMA'),
+	('Psicologos Bienestar - Sede Arequipa', 'Psicologos Bienestar - Sede Arequipa'),
+	('Centro de Salud Mental Comunitario Simon Bolivar', 'Centro de Salud Mental Comunitario Simon Bolivar'),
+	('Centro Psicologico de la Fuente', 'Centro Psicologico de la Fuente'),
+	('Centro Psicologico', 'Centro Psicologico'),
+	('Psicomedic-Tratamiento Psiquiatricos', 'Psicomedic-Tratamiento Psiquiatricos'),
+	('Centro Psicologico - NEURO TALENTO', 'Centro Psicologico - NEURO TALENTO'),
 )
 class Comentarios(models.Model):
 
 	ComCod = models.AutoField(primary_key=True)
 	nombreUser = models.CharField(max_length=100)
 	comentario = models.CharField(max_length=300)
-	centroAt = models.IntegerField(null=False, blank=False, choices=Centros)
+	centroAt = models.CharField(max_length=100, null=False, blank=False, choices=Centros)
 
 	class Meta:
 		verbose_name_plural = "Comentarios"
