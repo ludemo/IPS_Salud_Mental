5fbb53c64678066d987905663902a609c0522e6d
PauChoqueneira <pchoqueneira@unsa.edu.pe>
2021-07-28 12:49:21 -0500

    modelo Comentarios creado, implementacion de script para comentarios y modificacion de mapa.html (datos del modelo)

--- mapa/models.py
+++ mapa/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+Centros = (
+	(1, 'Hospital Honorio Delgado'),
+	(2, 'Hospital Goyeneche'),
+	(3, 'Clinica de Neuropsiquiatria y Salud Mental San Rafael'),
+	(4, 'Centro de Salud Mental Muñoz'),
+	(5, 'Centro de Salud Mental Comunitario Hunter'),
+	(6, 'El Instituto de Salud Mental Carlos Alberto Seguin Escobedo'),
+	(7, 'Centro de Adicciones Y Salud Mental'),
+	(8, 'Centro de salud mental comunitario San Martin de Porres Arequipa'),
+	(9, 'Clinica de Salud Mental y Adicciones'),
+	(10, 'Centro Psicologico Kid Zone'),
+	(11, 'Centro de Salud Mental y Adicciones Victor Raul Hinojosa'),
+	(12, 'Centro de Salud Mental Comunitario CAYMA'),
+	(13, 'Psicologos Bienestar - Sede Arequipa'),
+	(14, 'Centro de Salud Mental Comunitario Simon Bolivar'),
+	(15, 'Centro Psicologico de la Fuente'),
+	(16, 'Centro Psicologico'),
+	(17, 'Psicomedic-Tratamiento Psiquiatricos'),
+	(18, 'Centro Psicologico - NEURO TALENTO'),
+)
+class Comentarios(models.Model):
+
+	ComCod = models.AutoField(primary_key=True)
+	nombreUser = models.CharField(max_length=100)
+	comentario = models.CharField(max_length=300)
+	centroAt = models.IntegerField(null=False, blank=False, choices=Centros)
+
+	class Meta:
+		verbose_name_plural = "Comentarios"
--- mapa/views.py
+++ mapa/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from mapa.models import Comentarios
+from django.http import JsonResponse
 
 # Create your views here.
 class MapaView(TemplateView):
     template_name = "mapa.html" 
+    def get_context_data(self, **kwargs):
+        queryset = Comentarios.objects.all()
+        context = super().get_context_data(**kwargs)
+        context["Comentarios"] = queryset
+        #alright aqui traete los comentarios y metelo al context
+        return context 
+
+    def render_to_response(self, context, **response_kwargs):
+        if self.request.is_ajax():
+            queryData = context["Comentarios"].values()
+            data = list(queryData)
+
+            return JsonResponse({'Comentarios': data}) 
+        else:
+            response_kwargs.setdefault('content_type', self.content_type)
+            
+            return self.response_class(
+                request=self.request,
+                template=self.get_template_names(),
+                context=context,
+                using=self.template_engine,
+                **response_kwargs
+            )
+
